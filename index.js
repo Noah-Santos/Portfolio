43c5d4567541cbb3120e511d4eda7d15349695ac
Noah-Santos <nsanto591@west-mec.org>
2022-12-13 16:00:37 -0700

    Slideshow
    Changing towards slideshow for projects.

---
+++ index.js
@@ -0,0 +1,12 @@
+//array with all project links
+let projects = ["https://noah-santos.github.io/Convertion-Project/", "https://noah-santos.github.io/littlemissmia_personal/", "https://noah-santos.github.io/Tic-Tac-Toe/"]
+function slideshowChange(){
+    while(true){
+        if(time == 4){
+            document.getElementById
+        }
+    }
+}
+function increaseTime(time){
+    return time+=1;
+}
