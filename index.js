1a694892a06a86a3f4ce785913fb409dbab73c84
Noah-Santos <nsanto591@west-mec.org>
2022-12-14 10:33:59 -0700

    Slides
    Started on about me and resume. Slideshow almost works, just need to change the fact that sometimes it takes 2 clicks to move.

--- index.js
+++ index.js
@@ -2,17 +2,69 @@
 let projects = ["https://noah-santos.github.io/Convertion-Project/", "https://noah-santos.github.io/littlemissmia_personal/", "https://noah-santos.github.io/Tic-Tac-Toe/"]
 let images = ["convertionProject.PNG", "littlemissmiaProject.PNG", "tictactoe.PNG"];
 let i = 0;
-// // move the slides forward
+let direction="none";
+let tracker;
+// move the slides forward
+function slideIncrease(){
+    
+    if(direction=="none"){
+        i++;
+    }else if(direction=="left"){
+        i++;
+        if(0<=i<=2){
+            i=0;
+        }
+    }
+    direction="right";
+    document.getElementById("item1").href=projects[i];
+    document.getElementById("img1").src=images[i];
+    //sets the index to -1, so that the image will be the first one in the array
+    if(i==images.length-1){
+        i = -1;
+    }
+    document.getElementById("item2").href=projects[i+1];
+    document.getElementById("img2").src=images[i+1];
+    i++;
+    tracker = i;
+}
+//moves the slides back
+function slideDecrease(){
+    
+    if(direction=="right"){
+        i-=2;
+    }
+    direction="left";
+    i--;
+    //sets the index to 3, so that the image will be the last one in the array
+    if(i<=-1){
+        i = 2;
+    }
+    document.getElementById("item1").href=projects[i];
+    document.getElementById("img1").src=images[i];
+    if(i == images.length-1){
+        document.getElementById("item2").href=projects[i-2];
+        document.getElementById("img2").src=images[i-2];
+    }else{
+        document.getElementById("item2").href=projects[i+1];
+        document.getElementById("img2").src=images[i+1];
+    }
+    tracker =i;
+}
+
+
 // function slideIncrease(){
+//     i++;
 //     document.getElementById("item1").href=projects[i];
 //     document.getElementById("img1").src=images[i];
 //     //sets the index to -1, so that the image will be the first one in the array
+    
 //     if(i==images.length-1){
-//         i = -1;
+//         i = 0;
 //     }
 //     document.getElementById("item2").href=projects[i+1];
 //     document.getElementById("img2").src=images[i+1];
-//     i++;
+    
+//     console.log(i)
 // }
 // //moves the slides back
 // function slideDecrease(){
@@ -30,44 +82,10 @@ let i = 0;
 //         document.getElementById("item2").href=projects[i+1];
 //         document.getElementById("img2").src=images[i+1];
 //     }
-    
+
+
+//     console.log(i)
 // }
-
-
-function slideIncrease(){
-    i++;
-    document.getElementById("item1").href=projects[i];
-    document.getElementById("img1").src=images[i];
-    //sets the index to -1, so that the image will be the first one in the array
-    
-    if(i==images.length-1){
-        i = 0;
-    }
-    document.getElementById("item2").href=projects[i+1];
-    document.getElementById("img2").src=images[i+1];
-    
-    console.log(i)
-}
-//moves the slides back
-function slideDecrease(){
-    i--;
-    //sets the index to 3, so that the image will be the last one in the array
-    if(i==-1){
-        i = 2;
-    }
-    document.getElementById("item1").href=projects[i];
-    document.getElementById("img1").src=images[i];
-    if(i == images.length-1){
-        document.getElementById("item2").href=projects[i-2];
-        document.getElementById("img2").src=images[i-2];
-    }else{
-        document.getElementById("item2").href=projects[i+1];
-        document.getElementById("img2").src=images[i+1];
-    }
-
-
-    console.log(i)
-}
 
 // //move the slides forward
 // let tracker = 0
