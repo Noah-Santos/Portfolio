48c979e85b851b7f079b29d30eac182bfa20caf0
NoahSantos <nsanto591@west-mec.org>
2022-12-14 17:57:21 -0700

    Errors
    Starting new slideshow function.

--- index.js
+++ index.js
@@ -2,94 +2,19 @@
 let projects = ["https://noah-santos.github.io/Convertion-Project/", "https://noah-santos.github.io/littlemissmia_personal/", "https://noah-santos.github.io/Tic-Tac-Toe/"]
 let images = ["convertionProject.PNG", "littlemissmiaProject.PNG", "tictactoe.PNG"];
 let i = 0;
-let direction="none";
-let tracker;
-// move the slides forward
-function slideIncrease(){
-    
-    if(direction=="none"){
-        i++;
-    }else if(direction=="left"){
-        i++;
-        if(0<=i<=2){
-            i=0;
-        }
-    }
-    direction="right";
-    document.getElementById("item1").href=projects[i];
-    document.getElementById("img1").src=images[i];
-    //sets the index to -1, so that the image will be the first one in the array
-    if(i==images.length-1){
-        i = -1;
-    }
-    document.getElementById("item2").href=projects[i+1];
-    document.getElementById("img2").src=images[i+1];
-    i++;
-    tracker = i;
-}
-//moves the slides back
-function slideDecrease(){
-    
-    if(direction=="right"){
-        i-=2;
-    }
-    direction="left";
-    i--;
-    //sets the index to 3, so that the image will be the last one in the array
-    if(i<=-1){
-        i = 2;
-    }
-    document.getElementById("item1").href=projects[i];
-    document.getElementById("img1").src=images[i];
-    if(i == images.length-1){
-        document.getElementById("item2").href=projects[i-2];
-        document.getElementById("img2").src=images[i-2];
-    }else{
-        document.getElementById("item2").href=projects[i+1];
-        document.getElementById("img2").src=images[i+1];
-    }
-    tracker =i;
-}
-
-
+let first="none";
+// // move the slides forward
 // function slideIncrease(){
-//     i++;
-//     document.getElementById("item1").href=projects[i];
-//     document.getElementById("img1").src=images[i];
-//     //sets the index to -1, so that the image will be the first one in the array
     
-//     if(i==images.length-1){
-//         i = 0;
+//     if(direction=="none"){
+//         i++;
+//     }else if(direction=="left"){
+//         i++;
+//         if(0<=i<=2){
+//             i=0;
+//         }
 //     }
-//     document.getElementById("item2").href=projects[i+1];
-//     document.getElementById("img2").src=images[i+1];
-    
-//     console.log(i)
-// }
-// //moves the slides back
-// function slideDecrease(){
-//     i--;
-//     //sets the index to 3, so that the image will be the last one in the array
-//     if(i==-1){
-//         i = 2;
-//     }
-//     document.getElementById("item1").href=projects[i];
-//     document.getElementById("img1").src=images[i];
-//     if(i == images.length-1){
-//         document.getElementById("item2").href=projects[i-2];
-//         document.getElementById("img2").src=images[i-2];
-//     }else{
-//         document.getElementById("item2").href=projects[i+1];
-//         document.getElementById("img2").src=images[i+1];
-//     }
-
-
-//     console.log(i)
-// }
-
-// //move the slides forward
-// let tracker = 0
-// function slideIncrease(){
+//     direction="right";
 //     document.getElementById("item1").href=projects[i];
 //     document.getElementById("img1").src=images[i];
 //     //sets the index to -1, so that the image will be the first one in the array
@@ -98,16 +23,20 @@ function slideDecrease(){
 //     }
 //     document.getElementById("item2").href=projects[i+1];
 //     document.getElementById("img2").src=images[i+1];
-//     tracker = i;
 //     i++;
-    
 // }
 // //moves the slides back
 // function slideDecrease(){
-//     i=tracker;
     
+//     if(direction=="right"){
+//         i-=2;
+//     }else{
+//         i--;
+//     }
+//     direction="left";
+//     // i--;
 //     //sets the index to 3, so that the image will be the last one in the array
-//     if(i==-1){
+//     if(i<=-1){
 //         i = 2;
 //     }
 //     document.getElementById("item1").href=projects[i];
@@ -119,5 +48,27 @@ function slideDecrease(){
 //         document.getElementById("item2").href=projects[i+1];
 //         document.getElementById("img2").src=images[i+1];
 //     }
-//     i--
 // }
+
+function change(direction){
+    if(direction == "right"){
+        if(first == "none"){
+            i++;
+        }
+        if(i+1 < projects.length){
+            document.getElementById("item1").href=projects[i];
+            document.getElementById("img1").src=images[i];
+            i++;
+            document.getElementById("item1").href=projects[i];
+            document.getElementById("img1").src=images[i];
+        }else if(i < -1 || i+1 >= 3){
+            i = 0;
+            document.getElementById("item1").href=projects[i];
+            document.getElementById("img1").src=images[i];
+            i++;
+            document.getElementById("item1").href=projects[i];
+            document.getElementById("img1").src=images[i];
+        }
+
+    }
+}
